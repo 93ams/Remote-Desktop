41692e38dbd5e75194bd5fc857422c14cb7cf415
André Stielau <a.m.stielau@gmail.com>
2016-06-06 15:00:38 +0100

    some frontend apearing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,5 +2,5 @@
     Ficheiro de routes e handler da pagina inicial.
 */
 const Path = require("path");
-const index = Path.join(__dirname, "..", "frontend", "index.html");
+const index = Path.join(__dirname, "..", "frontend", "public", "index.html");
 module.exports = {method: "GET", path: "/", handler: { file: index } };
--- backend/routes/static.js
+++ backend/routes/static.js
@@ -2,5 +2,5 @@
     Ficheiro de routes e handler dos ficheiros estaticos.
 */
 var Path = require("path");
-const public_dir = Path.join(__dirname, "..", "frontend", "public");
-module.exports = {method: "GET", path: "/{param*}", handler: { directory: { path: public_dir } } };
+const static_dir = Path.join(__dirname, "..", "frontend", "public", "static");
+module.exports = {method: "GET", path: "/{param*}", handler: { directory: { path: static_dir } } };
