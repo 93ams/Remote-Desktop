d3e240f98762a7667e6b3b60a6e1e7d58f791b2e
André Stielau <a.m.stielau@gmail.com>
2016-06-07 14:41:28 +0100

    half working

--- backend/frontend/public/static/js/controllers/files.js
+++ backend/frontend/public/static/js/controllers/files.js
@@ -2,52 +2,143 @@
 
 var app = app || {};
 
-app.controller('FilesCtrl', ['$scope', '$http', FilesCtrl]);
+app.controller('FilesCtrl', ['$scope', '$http', '$window', FilesCtrl]);
 
-function getBack(file){
-    var path = file.split("/");
-    path.pop();
-    return path.join("/") || "/";
-}
-
-function FilesCtrl($scope, $http) {
+function FilesCtrl($scope, $http, $window) {
     $scope.files = [];
     $scope.filesToUpload = [];
     $scope.currentPath = "/";
     $scope.clipboard = { file: null, mode: null };
 
-    $scope.list = function(file){
-        var path;
-        if(file) { path = file.path + "/" + file.name }
-        if(!file || file.isDirectory){
-            $http.get( "/files",
-                { params: { "path": path || "" } }
-            ).then(function(response) {
-                $scope.currentPath = path || "/";
-                $scope.files = response.data.files;
-            }, function errorCallback(response) {
+    // Utils
+
+    function getBack(file){
+        var path = file.split("/");
+        path.pop();
+        return path.join("/") || "/";
+    }
+
+    function addFile(file){
+        if(($scope.currentPath == file.path)
+        || ($scope.currentPath == "/"
+        &&  file.path == "")){
+            $scope.files.push(file);
+        }
+    }
+
+    function removeFile(file){
+        if(($scope.currentPath == file.path)
+        || ($scope.currentPath == "/"
+        &&  file.path == "")){
+            for(var i=0; i<$scope.files.length; i++){
+                if($scope.files[i].name == file.name){
+                    $scope.files.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+
+    // Anchored Methods
+
+    $scope.list = function(path, name){
+        var full_path;
+        if(path == "/") { full_path = name; }
+        else {
+            if(name){ full_path = path + "/" + name; }
+            else { full_path = path; }
+        }
+        $http.get( "/files",
+            { params: { "path": full_path || "" } }
+        ).then(function(res) {
+            $scope.currentPath = full_path || "/";
+            $scope.files = res.data.files;
+        }, function errorCallback(res) {
+            console.log("error");
+        });
+    }
+
+    $scope.get = function(file){
+        if(file){
+            $http.get( "/files/get",
+                { params: { "path": file.path, "file": file.name } }
+            ).then(function(res) {
+                var type = res.headers("Content-type");
+                var data = new Blob([res.data], {"type": type});
+                var url = $window.URL || $window.webkitURL;
+                alert(url.createObjectURL(data));
+            }, function errorCallback(res) {
                 console.log("error");
             });
         }
     }
 
-    $scope.get = function(file){
-
-    }
-
     $scope.upload = function(){
+        var path = $scope.currentPath;
+        if(path == "/") { path = ""; }
 
+        var files = $scope.filesToUpload;
+        if(files.length>0){
+            for(var i=0; i<files.length; i++){
+                var file = files[i];
+                var fd = new FormData();
+                fd.append("path", path);
+                fd.append("overwrite", "false");
+                fd.append("file", file);
+                $http.post( "/files", fd, {
+                    transformRequest: angular.identity,
+                    headers: {'Content-Type': undefined}
+                }).then(function(res){
+                    var data = res.data;
+                    switch(data.code){
+                        case 200:
+                            addFile(data.file);
+                            break;
+                        case 409:
+                            if(confirm("File " + data.name + " already exists, do you want to overwrite it?")){
+                                fd.append("overwrite", "true");
+                                $http.post( "/files", fd, {
+                                    transformRequest: angular.identity,
+                                    headers: {'Content-Type': undefined}
+                                }).then(function(res){
+                                    data = res.data;
+                                    if(data.code == 200){
+                                        removeFile(data.file);
+                                        addFile(data.file);
+                                    }
+                                }, function errorCallback(res) {
+                                    console.log("error");
+                                });
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }, function errorCallback(res) {
+                    console.log("error");
+                });
+            }
+            angular.element( document.querySelector( '#files-input' ) )[0].value = "";
+        }
     }
 
     $scope.delete = function(file){
-
+        $http.delete( "/files",
+            { params: { "path": file.path, "file": file.name } }
+        ).then(function(res) {
+            var data = res.data;
+            if(data.code == 200){
+                removeFile(file);
+            }
+        }, function errorCallback(res) {
+            console.log("error");
+        });
     }
 
     $scope.rename = function(file){
-        var new_name = prompt("Rename: ");
+        var new_name = prompt("Rename: ", file.name);
         if(new_name){
-
-
+            
         }
     }
 
@@ -74,9 +165,15 @@ function FilesCtrl($scope, $http) {
 
     $scope.back = function(){
         $scope.currentPath = getBack($scope.currentPath);
-        if($scope.currentPath == "/"){ $scope.list(null); }
-        else { $scope.list($scope.currentPath); }
+        $scope.list($scope.currentPath);
     }
 
-    $scope.list(null);
+    $scope.newDir = function(){
+        var dir_name = prompt("New directory's name: ");
+        if(dir_name){
+
+        }
+    }
+
+    $scope.list("/");
 }
--- backend/routes/files.js
+++ backend/routes/files.js
@@ -4,17 +4,33 @@
 
 const Path = require("path");
 const Fs = require("fs");
+const Mime = require("mime");
 
 const files_dir = Path.join(__dirname, "..", "files");
 // Cria pasta de files, caso não exista
 if(!Fs.existsSync(files_dir)){ Fs.mkdirSync(files_dir); }
+//TODO: buscar settings e criar o resto das pastas de base
+
 
 // Utils
 
 //Verifica se o caminho tem ".." para evitar porcaria
 const checkForCheating = function(path){
-    var aux = path.split('/');
-    return (aux.indexOf('..') == -1);
+    if(path!=null){
+        var aux = path.split('/');
+        return (aux.indexOf('..') == -1);
+    }
+}
+
+const rmdir = function(dir_path){
+    if( Fs.existsSync(dir_path) ) {
+        Fs.readdirSync(dir_path).forEach(function(file){
+            var file_path = Path.join(dir_path, file);
+            if(Fs.lstatSync(file_path).isDirectory()) { rmdir(file_path); }
+            else { Fs.unlinkSync(file_path); }
+        });
+        Fs.rmdirSync(dir_path);
+    }
 }
 
 // Handlers
@@ -26,43 +42,92 @@ const files_handler = function(req, res) {
     if(checkForCheating(path))
         absolute_path = Path.join(files_dir, path);
 
-    Fs.readdir(absolute_path, function (err, files) {
-        if (err) throw err;
+    if (Fs.existsSync(absolute_path)) {
+        Fs.readdir(absolute_path, function (err, files) {
+            if (err) throw err;
 
-        var data = [];
-        files.forEach(function(file){
-            if (Fs.lstatSync(Path.join(absolute_path,file)).isDirectory()) {
-                data.push({ name: file, isDirectory: true, path: path });
-            } else {
-                var ext = Path.extname(file);
-                data.push({ name: file, ext: ext, isDirectory: false, path: path });
-            }
+            var data = [];
+            files.forEach(function(file){
+                if (Fs.lstatSync(Path.join(absolute_path,file)).isDirectory()) {
+                    data.push({ name: file, isDirectory: true, path: path });
+                } else {
+                    var ext = Path.extname(file);
+                    data.push({ name: file, ext: ext, isDirectory: false, path: path });
+                }
+            });
+            res({ code: 200, files: data });
         });
-        res({ code: 200, files: data });
-    });
+    } else {
+        res({ code: 404 });
+    }
 }
 
 const upload_files_handler = function(req, res) {
     var absolute_path = files_dir;
-    var path = req.query.path || "";
-
-    if(checkForCheating(path)){
-        
+    var data = req.payload;
+    var path = data.path;
+    var overwrite = data.overwrite;
+    var file = data.file;
+    if(file){
+        var file_name = file.hapi.filename;
+        if(checkForCheating(path) && checkForCheating(file_name)){
+            var file_path = Path.join(absolute_path, path, file_name);
+            if(Fs.existsSync(file_path) && overwrite == "false"){
+                res({ code: 409, name: file_name });
+            } else {
+                var pipe = Fs.createWriteStream(file_path);
+                pipe.on('error', (err) => {
+                    if(err) throw err;
+                });
+                file.pipe(pipe);
+                file.on('end', function (err) {
+                    if(err) throw err;
+                    var ext = Path.extname(file_name);
+                    var response = { name: file_name, ext: ext, isDirectory: false, path: path };
+                    res({ code: 200, file: response });
+                });
+            }
+        }
     }
-
-    res("upload file");
 }
 
 const rename_files_handler = function(req, res) {
-    res("rename file");
+
 }
 
 const delete_files_handler = function(req, res) {
-    res("delete file");
+    var path = req.query.path || "";
+    var file = req.query.file;
+    if(checkForCheating(path) && checkForCheating(file)){
+        var file_path = Path.join(path, file);
+        var absolute_path = Path.join(files_dir, file_path);
+        if (Fs.existsSync(absolute_path)) {
+            if(Fs.lstatSync(absolute_path).isDirectory()){ rmdir(absolute_path); }
+            else { Fs.unlinkSync(absolute_path); }
+            res({ code: 200, name: file });
+        } else {
+            res({ code: 404 });
+        }
+    }
 }
 
 const get_file_handler = function(req, res) {
-    res("get file");
+    var path = req.query.path || "";
+    var file = req.query.file;
+
+    if(checkForCheating(path) && checkForCheating(file)){
+        var file_path = Path.join(path, file);
+        var absolute_path = Path.join(files_dir, file_path);
+        if (Fs.existsSync(absolute_path)) {
+            Fs.readFile(absolute_path, function(err, data){
+                if(err) throw err;
+                res(data).header('Content-Type', Mime.lookup(file))
+                         .header("Content-Disposition", "attachment; filename=" + file)
+            });
+        } else {
+            res({ code: 404 });
+        }
+    }
 }
 
 const copy_file_handler = function(req, res) {
@@ -73,12 +138,17 @@ const move_file_handler = function(req, res) {
     res("move file");
 }
 
-const routes = [{ method: "GET",    path: "/files",        handler: files_handler        },
-                { method: "POST",   path: "/files",        handler: upload_files_handler },
-                { method: "PUT",    path: "/files",        handler: rename_files_handler },
-                { method: "DELETE", path: "/files",        handler: delete_files_handler },
-                { method: "GET",    path: "/files/get",    handler: get_file_handler     },
-                { method: "POST",   path: "/files/copy",   handler: copy_file_handler    },
-                { method: "POST",   path: "/files/move",   handler: get_file_handler     }];
+const new_dir_handler = function(req, res) {
+    res("new dir");
+}
+
+const routes = [{ method: "GET",    path: "/files",          handler: files_handler        },
+                { method: "POST",   path: "/files",          config: { handler: upload_files_handler, payload: { output: 'stream', parse: true, allow: 'multipart/form-data' } } },
+                { method: "PUT",    path: "/files",          handler: rename_files_handler },
+                { method: "DELETE", path: "/files",          handler: delete_files_handler },
+                { method: "GET",    path: "/files/get",      handler: get_file_handler     },
+                { method: "POST",   path: "/files/copy",     handler: copy_file_handler    },
+                { method: "POST",   path: "/files/move",     handler: get_file_handler     },
+                { method: "POST",   path: "/files/newdir",   handler: new_dir_handler     }];
 
 module.exports = routes;
